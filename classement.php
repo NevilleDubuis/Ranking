40a2002897d4e59680e3ae2332e98890a4d00092
Néville Dubuis <ndubuis@gmail.com>
2013-07-12 19:19:20 +0200

    ranking fo abbay 2013

--- classement.php
+++ classement.php
@@ -12,6 +12,7 @@
 <html lang="fr">
 
 <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<link href="styles/forms.css" rel="stylesheet" type="text/css" />
 	<link href="styles/style.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="scripts/forms.js"></script>

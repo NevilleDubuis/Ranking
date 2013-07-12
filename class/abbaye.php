7e7dccb550f18b1aae4dae2c3df05a342d87b7b7
Néville Dubuis <ndubuis@gmail.com>
2013-07-12 18:54:54 +0200

    ranking fo abbay 2013

--- class/abbaye.php
+++ class/abbaye.php
@@ -58,6 +58,8 @@ while ($data = mysql_fetch_array($id_person)) {
     }
 
     $sql .= ' AS total FROM Société_2013 WHERE id_shooter = '.$person.' ORDER BY total DESC  LIMIT 0 , 1';
+    echo $sql;
+
     $res = mysql_query($sql) or die ("Requéte invalide");
     if (mysql_num_rows($res) != 0) {
         $total=0;
